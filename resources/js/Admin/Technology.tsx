56343ba8cdcef152d0a828c31582caa221409e3b
Takaki <rittukk1@gmail.com>
2023-10-09 22:29:24 +0700

    [feat]tech list

--- resources/js/Admin/Technology.tsx
+++ resources/js/Admin/Technology.tsx
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+interface TechnologyData {
+    id: number;
+    name: string;
+}
 
 const Technology: React.FC = () => {
-    const [technology, setTechnology] = useState<Record<string, any>>([]);
+    const [technologyList, setTechnologyList] = useState<TechnologyData[]>([]);
 
-    const fetchTechnology = () => {
+    useEffect(() => {
         fetch("http://127.0.0.1:8000/api/technology")
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
+                setTechnologyList(data.technologyList);
             });
-    };
-    return <div></div>;
+    }, []);
+
+    return (
+        <div>
+            <ul>
+                {technologyList.map((TechnologyData: any, key: number) => (
+                    <li key={key}>
+                        {TechnologyData.id}.{TechnologyData.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Technology;
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminArticleController;
 use App\Http\Controllers\AdminWordController;
+use App\Http\Controllers\AdminTechnologyController;
 
 
 /*
